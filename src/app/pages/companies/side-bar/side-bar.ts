c36cbb729835ea7b9ea65f36702597b5ee0a7241
Hazem Alyaari <133292426+AlyaariHazem@users.noreply.github.com>
2025-09-08 23:58:47 +0300

    feat: Refactor sidebar to use dynamic company name and add ProfileDataService for profile management

--- src/app/pages/companies/side-bar/side-bar.ts
+++ src/app/pages/companies/side-bar/side-bar.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 import { map } from 'rxjs';
 import { ProfileService } from '../services/profile.service';
+import { ProfileDataService } from './profile.service';
 
 @Component({
   selector: 'app-side-bar',
@@ -13,10 +14,24 @@ import { ProfileService } from '../services/profile.service';
 })
 export class SideBar {
   logo$: any;
+  companyName: string = 'شركة التقنيات المتقدمة';
   constructor(private http: HttpClient, private toastr: ToastrService, private profileService: ProfileService) {
     this.logo$ = this.profileService.getProfile$().pipe(
       map(p => environment.apiBaseUrl + p.company_logo)
     );
+  }
+  profile:any;
+  profileDataService = inject(ProfileDataService);
+
+  ngOnInit(): void {
+     this.profileDataService.getProfile().subscribe({
+          next: (data:any) => {
+            this.profile = data.data.profile;
+            this.companyName = this.profile.company_name;
+            debugger;
+            console.log('Profile data:', this.profile);
+          }
+       });
   }
 
 }
