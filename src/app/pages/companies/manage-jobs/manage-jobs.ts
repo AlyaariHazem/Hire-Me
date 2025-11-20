309db74d49180262762324ad3a0e6e53ae701f8c
Hazem Alyaari <133292426+AlyaariHazem@users.noreply.github.com>
2025-11-21 02:06:34 +0300

    feat: remove unused CompaniesModule import from manage-jobs component

--- src/app/pages/companies/manage-jobs/manage-jobs.ts
+++ src/app/pages/companies/manage-jobs/manage-jobs.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { CompaniesModule } from "../companies-module";
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-manage-jobs',
-  imports: [CompaniesModule],
+  imports: [],
   templateUrl: './manage-jobs.html',
   styleUrl: './manage-jobs.scss'
 })
