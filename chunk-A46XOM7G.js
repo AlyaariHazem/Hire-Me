d1429e9933ecd5a7ff345764e9be65f871f6e44c
AlyaariHazem <AlyaariHazem@users.noreply.github.com>
2025-11-24 22:01:18 +0000

    deploy: b80089d3ab8713351e3e4bb2ad83d8df5353b840

--- chunk-A46XOM7G.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-4KN2N3VH.js";import"./chunk-KT2HVTP4.js";import"./chunk-OVMELZYD.js";import"./chunk-6A5Y5MPN.js";import"./chunk-L43F6PFV.js";export{a as JobDetails};
---
+++ chunk-D2D32HX5.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-FNER2BHP.js";import"./chunk-KT2HVTP4.js";import"./chunk-OVMELZYD.js";import"./chunk-6A5Y5MPN.js";import"./chunk-L43F6PFV.js";export{a as JobDetails};
