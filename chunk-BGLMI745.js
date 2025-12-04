6a3cac2093c33fb9ad84c064ca256d2253c7bdf6
AlyaariHazem <AlyaariHazem@users.noreply.github.com>
2025-12-04 22:13:13 +0000

    deploy: dd3d078a86a7c868d46cc8198080f4e52cc171af

---
+++ chunk-BGLMI745.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-RTKADHQY.js";import"./chunk-NK7SDFXN.js";import"./chunk-HZGGMTPS.js";import"./chunk-OVMELZYD.js";import"./chunk-7IBYSJKF.js";import"./chunk-L43F6PFV.js";export{a as JobDetails};
